cd0088127824d20e59c4bcd66c675ce9ae2d0b66
Oleksandr <oleksandrkravets1995@gmail.com>
2020-05-01 00:27:56 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+'use strict'
+const items = document.querySelectorAll('.list-items');
+const modal = document.querySelector('#modal');
+const firstPage = document.querySelector('.first-page');
+const secondPage = document.querySelector('.second-page');
+const button = document.querySelector('.button');
+const numberInstagram = document.querySelector('#numberInstagram');
+const numberFacebook = document.querySelector('#numberFacebook');
+const numberGoogle = document.querySelector('#numberGoogle');
+const numberZpolecenia = document.querySelector('#numberZpolecenia');
+const numberInne = document.querySelector('#numberInne');
+
+numberInstagram.innerHTML = localStorage.getItem('Instagram');
+numberFacebook.innerHTML = localStorage.getItem('Facebook');
+numberGoogle.innerHTML = localStorage.getItem('Google');
+numberZpolecenia.innerHTML = localStorage.getItem('Z polecenia');
+numberInne.innerHTML = localStorage.getItem('Inne');
+
+items.forEach((item) => {
+    item.addEventListener('click', () => {
+        for(let i=0; i<localStorage.length; i++) {
+            let key = localStorage.key(i);
+            if (key === item.dataset.name) {
+                let number = +localStorage.getItem(key);
+                ++number;
+                localStorage.setItem(key,number);
+                numberInstagram.innerHTML = localStorage.getItem('Instagram');
+                numberFacebook.innerHTML = localStorage.getItem('Facebook');
+                numberGoogle.innerHTML = localStorage.getItem('Google');
+                numberZpolecenia.innerHTML = localStorage.getItem('Z polecenia');
+                numberInne.innerHTML = localStorage.getItem('Inne');
+            }
+          }
+         setTimeout(() => {
+            button.classList.add('disabled')
+            firstPage.classList.add('disabled');
+            modal.classList.remove('disabled');
+                setTimeout(() => {
+                button.classList.remove('disabled')
+                firstPage.classList.remove('disabled');
+                modal.classList.add('disabled');
+            }, 2000)
+          }, 100)
+    })
+})
+
+const showSecondPage = () => {
+    button.classList.add('red')
+    firstPage.classList.add('disabled');
+    secondPage.classList.remove('disabled');
+    button.innerHTML = 'BACK'
+    button.removeEventListener('click', showSecondPage);
+    button.addEventListener('click', showFirstPage);
+}
+
+const showFirstPage = () => {
+    button.classList.remove('red')
+    firstPage.classList.remove('disabled');
+    secondPage.classList.add('disabled');
+    button.innerHTML = 'INFO'
+    button.addEventListener('click', showSecondPage);
+    button.removeEventListener('click', showFirstPage);
+}
+
+button.addEventListener('click', showSecondPage);
+
+
